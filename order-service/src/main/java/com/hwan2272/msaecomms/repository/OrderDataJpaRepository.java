c7d67afc457e24a83debfaf2ad168bb3e4d5cfa1
hwan2272 <hwan2230@naver.com>
2022-05-26 13:57:44 +0900

    * msaecomms - Data JPA 관련 보충

--- order-service/src/main/java/com/hwan2272/msaecomms/repository/OrderDataJpaRepository.java
+++ order-service/src/main/java/com/hwan2272/msaecomms/repository/OrderDataJpaRepository.java
@@ -1,11 +1,29 @@
 package com.hwan2272.msaecomms.repository;
 
 import com.hwan2272.msaecomms.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface OrderDataJpaRepository extends JpaRepository<OrderEntity, Long> {
     public Iterable<OrderEntity> findOrdersByUserId(String userId);
     public OrderEntity findByOrderId(String orderId);
+
+    @Query("select o from OrderEntity o where o.userId= :userId and o.productId= :productId")
+    public Iterable<OrderEntity> findOrders2Condition(
+            @Param("userId") String userId, @Param("productId") String productId);
+
+    @Query("select o from OrderEntity o where o.userId in :userIds")
+    public Iterable<OrderEntity> findOrders1CollectCondition(
+            @Param("userIds") Collection<String> userIds);
+
+    Page<OrderEntity> findOrdersPageByUserId(String userId, Pageable pageable);
+    Slice<OrderEntity> findOrdersSliceByUserId(String userId, Pageable pageable);
 }
--- order-service/src/test/java/com/hwan2272/msaecomms/entity/OrderEntityTest.java
+++ order-service/src/test/java/com/hwan2272/msaecomms/entity/OrderEntityTest.java
@@ -3,16 +3,23 @@ package com.hwan2272.msaecomms.entity;
 import com.hwan2272.msaecomms.repository.OrderDataJpaRepository;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
 
 @SpringBootTest
 @Transactional
@@ -35,6 +42,46 @@ class OrderEntityTest {
         orderEntity.setCreatedAt(new Date());
 
         orderDataJpaRepository.save(orderEntity);
+    }
+
+    @Test
+    public void testEntity2() {
+        String userId= "user1111";
+        String productId= "P-001";
+        Iterable<OrderEntity> orders = orderDataJpaRepository.findOrders2Condition(userId, productId);
+        System.out.println(String.format("orders=%s", orders.toString()));
+    }
+
+    @Test
+    public void testEntity3() {
+        String userId= "user1111";
+        List<String> userIds = new ArrayList<>();
+        userIds.add(userId);
+
+        Iterable<OrderEntity> orders = orderDataJpaRepository.findOrders1CollectCondition(userIds);
+        System.out.println(String.format("orders=%s", orders.toString()));
+    }
+
+    //@Test
+    //public void testEntity4() {
+        //return type optional = 단건, 값이 있을지 없을지 모른다
+        //기타: url참조 = https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repository-query-return-types
+    //}
+
+    @Test
+    public void testEntity5Paging() {
+        int page = 1;
+        int size = 10;
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        Page<OrderEntity> page1 = orderDataJpaRepository.findOrdersPageByUserId("user1111", pageRequest);
+
+        List<OrderEntity> content = page1.getContent();
+        long totalElements = page1.getTotalElements();
+
+        //System.out.println("content = " + content);
+        assertThat(page1.getTotalElements() == 3);
+        assertThat(page1.getContent().get(0).getUserId().equals("user1111"));
     }
 
 }
