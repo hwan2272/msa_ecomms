8cd2aa87bb1d4b799f76ff6427a8563797f42da4
hwan2272 <hwan2230@naver.com>
2022-05-26 15:56:46 +0900

    * msaecomms - Data JPA 관련 보충2

--- order-service/src/main/java/com/hwan2272/msaecomms/repository/OrderDataJpaRepository.java
+++ order-service/src/main/java/com/hwan2272/msaecomms/repository/OrderDataJpaRepository.java
@@ -4,12 +4,16 @@ import com.hwan2272.msaecomms.entity.OrderEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.QueryHint;
 import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface OrderDataJpaRepository extends JpaRepository<OrderEntity, Long> {
@@ -25,5 +29,26 @@ public interface OrderDataJpaRepository extends JpaRepository<OrderEntity, Long>
             @Param("userIds") Collection<String> userIds);
 
     Page<OrderEntity> findOrdersPageByUserId(String userId, Pageable pageable);
-    Slice<OrderEntity> findOrdersSliceByUserId(String userId, Pageable pageable);
+
+    //Slice<OrderEntity> findOrdersSliceByUserId(String userId, Pageable pageable);
+
+    //fetchJoin - N+1 문제 솔루션
+    @Query("select o from OrderEntity o inner join fetch o.user")
+    public OrderEntity findOrdersFetchJoin();
+
+    //fetchJoin - EntityGraph - Data Jpa용
+    @Override
+    @EntityGraph(attributePaths = {"user"})
+    public List<OrderEntity> findAll();
+
+    @EntityGraph(attributePaths = {"user"})
+    @Query("select o from OrderEntity o inner join fetch o.user")
+    public OrderEntity findOrdersEntityGraph();
+
+    //readOnly로만 작동
+    @QueryHints(value = @QueryHint(name= "org.hibernate.readOnly", value = "true"))
+    public OrderEntity findOrderReadOnly();
+
+
+
 }
